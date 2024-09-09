47ca626de29e1fe414fdf9e175dfec43759c5bcd
sharif57 <sharifmahamud577951@gmail.com>
2024-09-09 12:38:51 +0600

    footer section done

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,8 +1,14 @@
-import Home from "../page/Home";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Main = () => {
   return <div>
-    <Home></Home>
+    <Navbar></Navbar>
+    <div className="h-screen">
+        <Outlet></Outlet>
+    </div>
+    <Footer></Footer>
   </div>;
 };
 export default Main;
